8a9539c2a76b2b31b51282d859de94b20c93dbd2
Scott Schipke <swschipke@gmail.com>
2019-07-05 20:04:36 -0600

    Display curent guesses and players' names on bottom

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ var nameOne = document.querySelector('.section2__name1--input');
 var nameTwo = document.querySelector('.section2__name2--input');
 var resetBtn = document.querySelector('.section2__reset--btn');
 var submitGuess = document.querySelector('.section2__submit--btn');
+var challenger1Name = document.querySelector('.article1__h4');
+var challenger2Name = document.querySelector('.article2__h4');
+var challenger1Name = document.querySelector('.article1__h4');
+var challenger2Name = document.querySelector('.article2__h4');
+var guess1Out = document.querySelector('.article1__out');
+var guess2Out = document.querySelector('.article2__out');
 
 getRandomNumber();
 enableClearButton();
@@ -16,6 +22,11 @@ guessTwo.addEventListener('keyup',enableClearButton);
 nameOne.addEventListener('keyup',enableClearButton);
 clearBtn.addEventListener('click', clearGame);
 clearBtn.addEventListener('click', enableClearButton);
+resetBtn.addEventListener('click', resetGame);
+resetBtn.addEventListener('click', enableClearButton);
+resetBtn.addEventListener('click', getRandomNumber);
+submitGuess.addEventListener('click', submit)
+
 
 
 function getRandomNumber(min, max) {
@@ -33,8 +44,7 @@ function enableClearButton() {
           clearBtn.disabled = true;
           clearBtn.classList.add('disabled');
           resetBtn.disabled=true;
-          resetBtn.classList.add('disabled');
-          console.log(inputFields);
+          resetBtn.classList.add('disabled')
           return;
      }
      else {
@@ -49,6 +59,24 @@ function clearGame() {
      guessOne.value = null;
      guessTwo.value = null;
 }
+
+function resetGame() {
+     minRange.value = null;
+     maxRange.value = null;
+     guessOne.value = null;
+     guessTwo.value = null;
+     nameOne.value = null;
+     nameTwo.value = null;
+}
+
+function submit() {
+     challenger1Name.innerText = nameOne.value;
+     challenger2Name.innerText = nameTwo.value;
+     guess1Out.innerText = guessOne.value;
+     guess2Out.innerText = guessTwo.value;
+
+}
+
 
 
 
