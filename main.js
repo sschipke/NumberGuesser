0d3cd601b5ad0ae976a63dc2b738bbf70f391f70
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-07-08 10:21:36 -0600

    Add fucntion to card, add css to card

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ var errorMessage2 = document.querySelector('.guess__error2');
 var span1 = document.querySelector('.section2__span1');
 var span2 = document.querySelector('.section2__span2');
 var rangeErrorMin = document.querySelector('.error__minRange');
+var winnerCardDisplay = document.querySelector('.main__right');
 var randomNum = null;
 getRandomNumber();
 enableClearButton();
@@ -42,21 +43,17 @@ submitGuess.addEventListener('click', displayGuessErr2);
 submitGuess.addEventListener('click', compareNumbers1);
 submitGuess.addEventListener('click', compareNumbers2);
 upDateBtn.addEventListener('click', updateRangeInputs);
-upDateBtn.addEventListener('click', displayRangeErr);
-
 
 /************Functions***************/
 
-function getRandomNumber(min, max) {
-  var min = Math.ceil(min);
-  var max = Math.floor(max);
+function getRandomNumber() {
   randomNum = Math.floor((Math.random() * 100) + 1);
   return randomNum;
 }
 console.log(randomNum)
 
 function enableClearButton() {
-  var inputFields = [guessOne.value, guessTwo.value, nameOne.value, guessTwo.value];
+  var inputFields = [guessOne.value, guessTwo.value, nameOne.value, nameTwo.value];
   if (inputFields.includes('')) {
     clearBtn.disabled = true;
     clearBtn.classList.add('disabled');
@@ -93,56 +90,66 @@ function submit() {
   guess2Out.innerText = guessTwo.value;
   displayGuessErr1();
   displayGuessErr2();
+  isNotANumber();
 }
 
-function isNotANumber(num) {
-  return isNaN(parseInt(num));
+function isNotANumber() {
+  if (guessOne.value === 'e') {
+    console.log(guessOne.value)
+    errorMessage1.innerText = "Error: Not a number!";
+    errorMessage1.hidden = false;
+  } else {
+    errorMessage1.hidden = true;
+  }
 }
 
- function compareNumbers1() {
-      if (parseInt(guessOne.value) < randomNum) { guessMessage1.innerText = "That's too low!"
-     } else if (parseInt(guessOne.value) > randomNum) {
-          guessMessage1.innerText = "That's too high!"
-     } else if (parseInt(guessOne.value) === randomNum) {
-          guessMessage1.innerText = 'BOOM!'
-     }
- }
+function compareNumbers1() {
+  if (parseInt(guessOne.value) < randomNum) { guessMessage1.innerText = "That's too low!"
+  } else if (parseInt(guessOne.value) > randomNum) {
+    guessMessage1.innerText = "That's too high!"
+  } else if (parseInt(guessOne.value) === randomNum) {
+    guessMessage1.innerText = "BOOM!"
+    appendWinnerCard();
+  }
+}
 
 function compareNumbers2() {
-     if (parseInt(guessTwo.value) < randomNum) {
-     guessMessage2.innerText = "That's too low!"
-     } else if (parseInt(guessTwo.value) > randomNum) {
-          guessMessage2.innerText = "That's too high!"
-     } else if (parseInt(guessTwo.value) === randomNum) {
-          guessMessage2.innerText = 'BOOM!'
-     }
+  if (parseInt(guessTwo.value) < randomNum) {
+    guessMessage2.innerText = "That's too low!"
+  } else if (parseInt(guessTwo.value) > randomNum) {
+    guessMessage2.innerText = "That's too high!"
+  } else if (parseInt(guessTwo.value) === randomNum) {
+    guessMessage2.innerText = "BOOM!"
+    appendWinnerCard();  
+  }
 }
 
 function updateRangeInputs() {
   var min = parseInt(minRange.value);
   var max = parseInt(maxRange.value);
-   randomNum = Math.floor((Math.random() * (max - min + 1)) + min);
-   console.log('random number', randomNum);
-   displayRange();
-   return randomNum;
+  randomNum = Math.floor((Math.random() * (max - min + 1)) + min);
+  console.log('random number', randomNum);
+  displayRange(); 
+  displayRangeErr();
+  return randomNum;
 }
 
 function displayGuessErr1() {
   if (parseInt(guessOne.value) < parseInt(minRange.value)) {
-    errorMessage1.hidden = false
+    errorMessage1.hidden = false;
   } else if (
     parseInt(guessOne.value) > parseInt(maxRange.value)) {
-      errorMessage1.hidden = false
+      errorMessage1.hidden = false;
     } else (errorMessage1.hidden = true)
 }
-
+    
 function displayGuessErr2() {
   if (parseInt(guessTwo.value) < parseInt(minRange.value)) {
-    errorMessage2.hidden = false
+    errorMessage2.hidden = false;
   } else if (
     parseInt(guessTwo.value) > parseInt(maxRange.value)) {
-      errorMessage2.hidden = false
-    } else (errorMessage2.hidden = true)
+    errorMessage2.hidden = false
+  } else (errorMessage2.hidden = true)
 }
 
 function displayRange() {
@@ -153,6 +160,31 @@ function displayRange() {
 function displayRangeErr() {
   if (parseInt(minRange.value) > parseInt(maxRange.value)) {
     rangeErrorMin.hidden = false;
-  } else {rangeErrorMin.hidden = true 
-    }
+    rangeErrorMin.innerText = "Invalid Range!"
+  } else if (minRange.value === "" || maxRange.value === "") {
+    rangeErrorMin.innerText = "Please enter two values!"
+    rangeErrorMin.hidden = false;
+  } else {
+    rangeErrorMin.hidden = true; 
+  }
+}
+
+function appendWinnerCard() {
+  if (guessMessage1.innerText === "BOOM!" || guessMessage2.innerText === "BOOM!") {
+  winnerCardDisplay.insertAdjacentHTML(
+    "afterbegin", `<article class="section__right--art1">
+      <div class="art1__paragraphs">
+        <p class="art1__p--name1">CHALLENGER 1 NAME</p>
+        <p class="art1__p--vs"> vs </p>
+        <p class="art1__p3--name2">CHALLENGER 2 NAME</p>
+      </div>
+      <h5 class="art1__h5--name">CHALLENGER NAME</h5>
+      <h5 class="art1__h5--winner">WINNER</h5>
+      <div class="art1__displyinfo">
+        <p class="art1__guessNumber">XGUESSES</p>
+        <p class="art1__minTime">XMINUTES</p>
+        <button class="art1__deletebtn">X</button>
+      </div>
+    </article>`);
+  }
 }
